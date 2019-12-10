1a4c1df7801510a0a366bec955aa2c7500b7ce8b
Snash <raulmv01@hotmail.es>
2019-12-10 18:42:55 +0100

    config set and mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const db = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
+//Connect to Database
+db();
+
 
 const app = express(  
   () => console.log('Server up and running')
