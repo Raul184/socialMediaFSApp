b7932ffd6ab6101229f309de32d0ac9599f3c723
Snash <raulmv01@hotmail.es>
2019-12-10 19:54:14 +0100

    routing added

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const db = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
+
+const app = express();
 //Connect to Database
 db();
 
+app.get('/' , ( req , res ) => res.send('API running'));
+// Defined routes
+app.use('/api/users' , require('./routes/api/users'))
+app.use('/api/auth' , require('./routes/api/auth'))
+app.use('/api/profile' , require('./routes/api/profile'))
+app.use('/api/posts' , require('./routes/api/posts'))
 
-const app = express(  
-  () => console.log('Server up and running')
-).listen(PORT);
+
+
+
+app.listen(PORT);
 
