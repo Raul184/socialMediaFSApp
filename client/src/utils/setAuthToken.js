3f075b0ac3b73f60db3540a9016c0da58aebfb75
Snash <raulmv01@hotmail.es>
2019-12-18 15:05:30 +0100

    Register user route completed

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const setAuthToken = token => {
   if(token){
-    axios.defaults.headers['x-auth-token'] = token;
+    return axios.defaults.headers['x-auth-token'] = token;
   }
   else{
-    delete axios.defaults.headers.common['x-auth-token'];
+    return delete axios.defaults.headers.common['x-auth-token'];
   }
 }
 
